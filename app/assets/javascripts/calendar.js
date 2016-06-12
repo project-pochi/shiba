9a229e9ac78282856a5013bbde6af085dda1c975
sojiro14 <sojiro@cpan.org>
2016-06-13 05:33:10 +0900

    add calendar

---
+++ app/assets/javascripts/calendar.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    $('#calendar').fullCalendar({
+        // put your options and callbacks here
+      events: [ { "id": 1, "title": "test", "allDay": false, "start": "2016-06-12T19:09:16", "end": "2016-06-15T19:09:16" } ],
+      selectable: true
+    })
+
+});
