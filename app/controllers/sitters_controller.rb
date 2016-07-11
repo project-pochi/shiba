60f13566db393f69a911b4ce94654475e25b32a8
sojiro14 <sojiro@cpan.org>
2016-07-12 04:29:11 +0900

    add search by calendar

--- app/controllers/sitters_controller.rb
+++ app/controllers/sitters_controller.rb
@@ -3,13 +3,35 @@ class SittersController < ApplicationController
   def index
     query = {}
 
-    if params[:zip_code] && zip_code = params[:zip_code].match(/\A\d{3}\-\d{4}\z/)
-      query[:zip_code] = zip_code[0]
+    if params[:zip_code].present? && zip_code = params[:zip_code].match(/\A\d{3}\-\d{4}\z/)
       @locations = Location.near(zip_code[0], 1, :select => 'zip_code')
       logger.debug @locations.inspect
-      @sitters = Sitter.where(:zip_code => @locations.map(&:zip_code))
-      logger.debug @sitters.inspect
+
+      query[:zip_code] = @locations.map(&:zip_code)
     end
+
+    if params[:start].present? && params[:end].present?
+      request_dates = (Date.parse(params[:start])..Date.parse(params[:end]))
+      logger.debug request_dates.inspect
+      @sitter_busy_dates = SitterBusyDate.where(:start => request_dates)
+      logger.debug @sitter_busy_dates.inspect
+
+      query[:busy_sitter_id] = @sitter_busy_dates.map(&:sitter_id)
+    end
+
+    @sitters = nil
+    if query[:zip_codes] && query[:busy_sitter_id]
+      @sitters = Sitter.where(:zip_code => query[:zip_code]).where.not(:id => query[:busy_sitter_id])
+    elsif query[:zip_codes]
+      @sitters = Sitter.where(:zip_code => query[:zip_code])
+    elsif query[:busy_sitter_id]
+      @sitters = Sitter.where.not(:id => query[:busy_sitter_id])
+    else
+      @sitters = Sitter.all
+    end
+
+    #@sitters = Sitter.where(:zip_code => @locations.map(&:zip_code))
+    logger.debug @sitters.inspect
 
     @users = User.where(:id => @sitters.map(&:user_id))
   end
